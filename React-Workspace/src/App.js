d3d47437d9f3d8c31b33037b10cd8b514651741a
giantnerd123 <payneskyler@yahoo.com>
2022-05-20 16:31:09 -0700

    added homepage and profile banner template

--- React-Workspace/src/App.js
+++ React-Workspace/src/App.js
@@ -1,10 +1,15 @@
 import './index.css';
 import Example from './components/Example';
+import HomePageFeed from './pages/HomepageFeed';
+import ProfileBanner from './components/ProfileBanner';
 
 function App() {
   return (
     <div>
-      <Example />
+      {/* <Example /> */}
+      <HomePageFeed />
+      <ProfileBanner />
+
     </div>
   );
 }
