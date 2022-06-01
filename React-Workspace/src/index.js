aebc9e5da74fa70c2d464c08eb21a607df9b774b
Skyler Payne <88413851+giantnerd123@users.noreply.github.com>
2022-06-01 15:28:37 -0700

    Revert "Profile Page "

--- React-Workspace/src/index.js
+++ React-Workspace/src/index.js
@@ -3,22 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose} from 'redux';
-import thunk from 'redux-thunk';
-import reducers from './reducers';
-
-const store = createStore(reducers, compose(applyMiddleware(thunk)))
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>
-  // <React.StrictMode>
-      
-  // </React.StrictMode>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
