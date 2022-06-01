9a8b82ef37a135dde53c98303246fc3befc196c1
Caesar11SL <97268174+Caesar11SL@users.noreply.github.com>
2022-06-01 15:33:46 -0700

    updated

--- React-Workspace/src/components/Calories/calories.js
+++ React-Workspace/src/components/Calories/calories.js
@@ -2,15 +2,19 @@ import React, {useState, useEffect} from "react";
 import axios from 'axios';
 import SearchResult from './sub-cal/Searchresult'
 import SearchForm from './sub-cal/SearchForm'
+// import dotenv from 'dotenv';
 
-
+// dotenv.config();
 
 const Calories = () => {
   const [apiData, setApiData] = useState([]);
   const [searchString, setSearchString] = useState('');
   
   function getItem(searchString) {
-    const url = `https://api.nutritionix.com/v1_1/search/${searchString}?results=0:20&fields=item_name,brand_name,item_id,nf_calories&appId=631a2264&appKey=472c2175b7f61cb075c3015f90f5c274&limit=10`;
+    const appId = process.env.appId;
+    const appKey = process.env.appKey;
+
+    const url = `https://api.nutritionix.com/v1_1/search/${searchString}?results=0:20&fields=item_name,brand_name,item_id,nf_calories&appId=${appId}&appKey=${appKey}&limit=10`;
     
     axios.request(url)
     // .then(res => console.log(res.data))
--- React-Workspace/src/pages/Profile.js
+++ React-Workspace/src/pages/Profile.js
@@ -61,7 +61,12 @@ export default function Profile() {
           </div>
           <div className="mt-6 sm:flex-1 sm:min-w-0 sm:flex sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
             <div className="sm:hidden md:block mt-6 min-w-0 flex-1">
-              <h1 className="text-2xl font-bold text-gray-900 truncate">{user.result.name}</h1>
+              <h1 className="text-4xl font-bold text-gray-900 truncate">{user.result.name}</h1>
+            </div>
+            <div>
+            <button href="#" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Edit
+            </button>
             </div>
           </div>
         </div>
