16f9fc4f60c4cf0749d5df46b60a18b3a0aa06fd
jraju01 <jamesraju91@gmail.com>
2022-06-03 18:39:24 -0400

    rendered images

--- React-Workspace/src/components/Recipes/Recipe/Recipe.js
+++ React-Workspace/src/components/Recipes/Recipe/Recipe.js
@@ -3,6 +3,7 @@ import moment from 'moment';
 import { useDispatch } from 'react-redux';
 import { createRecipe, updateRecipe, deleteRecipe} from '../../../actions/recipes';
 import Recipes from '../Recipes';
+import { PaperClipIcon } from '@heroicons/react/solid'
 
 import { Menu, Popover, Transition } from '@headlessui/react'
 import {
@@ -31,6 +32,78 @@ const dispatch = useDispatch();
 const user = JSON.parse(localStorage.getItem('profile'));
 
   return (
+
+    /* This example requires Tailwind CSS v2.0+ */
+
+
+// export default function Example() {
+//   return (
+//     <div className="bg-white shadow overflow-hidden sm:rounded-lg">
+//       <div className="px-4 py-5 sm:px-6">
+//         <h3 className="text-lg leading-6 font-medium text-gray-900">Applicant Information</h3>
+//         <p className="mt-1 max-w-2xl text-sm text-gray-500">Personal details and application.</p>
+//       </div>
+//       <div className="border-t border-gray-200">
+//         <dl>
+//           <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+//             <dt className="text-sm font-medium text-gray-500">Full name</dt>
+//             <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">Margot Foster</dd>
+//           </div>
+//           <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+//             <dt className="text-sm font-medium text-gray-500">Application for</dt>
+//             <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">Backend Developer</dd>
+//           </div>
+//           <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+//             <dt className="text-sm font-medium text-gray-500">Email address</dt>
+//             <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">margotfoster@example.com</dd>
+//           </div>
+//           <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+//             <dt className="text-sm font-medium text-gray-500">Salary expectation</dt>
+//             <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">$120,000</dd>
+//           </div>
+//           <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+//             <dt className="text-sm font-medium text-gray-500">About</dt>
+//             <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+//               Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur
+//               qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia proident. Irure nostrud
+//               pariatur mollit ad adipisicing reprehenderit deserunt qui eu.
+//             </dd>
+//           </div>
+//           <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+//             <dt className="text-sm font-medium text-gray-500">Attachments</dt>
+//             <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+//               <ul role="list" className="border border-gray-200 rounded-md divide-y divide-gray-200">
+//                 <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
+//                   <div className="w-0 flex-1 flex items-center">
+//                     <PaperClipIcon className="flex-shrink-0 h-5 w-5 text-gray-400" aria-hidden="true" />
+//                     <span className="ml-2 flex-1 w-0 truncate">resume_back_end_developer.pdf</span>
+//                   </div>
+//                   <div className="ml-4 flex-shrink-0">
+//                     <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+//                       Download
+//                     </a>
+//                   </div>
+//                 </li>
+//                 <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
+//                   <div className="w-0 flex-1 flex items-center">
+//                     <PaperClipIcon className="flex-shrink-0 h-5 w-5 text-gray-400" aria-hidden="true" />
+//                     <span className="ml-2 flex-1 w-0 truncate">coverletter_back_end_developer.pdf</span>
+//                   </div>
+//                   <div className="ml-4 flex-shrink-0">
+//                     <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+//                       Download
+//                     </a>
+//                   </div>
+//                 </li>
+//               </ul>
+//             </dd>
+//           </div>
+//         </dl>
+//       </div>
+//     </div>
+//   )
+// }
+
     <div>
       <div>
                                 <a href={recipe.id} to={recipe.id} className="hover:underline">
@@ -38,16 +111,36 @@ const user = JSON.parse(localStorage.getItem('profile'));
                                 </a>
       </div>
       <div>
-        {recipe.name}
+        
+        <h1>Recipe Name: {recipe.name}</h1>
       </div>
+
+
+      <br />
+
+
       <div>
-        {recipe.ingredients}
+
+      <h1>Ingredients: {recipe.ingredients}</h1>
+        
       </div>
+
+
+      <br />
+
+
       <div>
-        {recipe.instructions}
+      <h1>Instructions: {recipe.instructions}</h1>
+        
       </div>
+
+
+      <br />
+
+      
       <div>
-        {recipe.img}
+       <img src={recipe.img}></img> 
+        {/* <img src={image} height={100} width={100} /> */}
       </div>
       {(user?.result?._id === recipe?.creator) && (
         <div>
