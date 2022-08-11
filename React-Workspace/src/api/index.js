67238ecd94968d8730fe506ed552c513ccdb5eab
giantnerd123 <payneskyler@yahoo.com>
2022-08-10 22:43:44 -0700

    changed to heroku

--- React-Workspace/src/api/index.js
+++ React-Workspace/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({baseURL: 'http://localhost:5001'})
+const API = axios.create({baseURL: 'https://whispering-refuge-62936.herokuapp.com/'})
 
 API.interceptors.request.use((req)=>{
     if(localStorage.getItem('profile')){
